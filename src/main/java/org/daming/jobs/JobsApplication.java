251695df471cdb589f55eb62d5f2e10a29445fd6
Arthur Ming <guobin.ming@pwc.com>
2020-10-31 10:07:27 +0800

    style: remove useless import statement

--- src/main/java/org/daming/jobs/JobsApplication.java
+++ src/main/java/org/daming/jobs/JobsApplication.java
@@ -1,20 +1,9 @@
 package org.daming.jobs;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.ansi.AnsiOutput;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.SpringVersion;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 @EnableOpenApi
