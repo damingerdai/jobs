6a93ebeea02d9810ceaad331da7f6c8c60758cd1
大ædamingerdai <mingguobin@live.com>
2020-11-08 22:09:52 +0800

    feat: imporve logger based on logback

--- src/main/java/org/daming/jobs/base/constant/CommonConstant.java
+++ src/main/java/org/daming/jobs/base/constant/CommonConstant.java
@@ -2,7 +2,14 @@ package org.daming.jobs.base.constant;
 
 public class CommonConstant {
 
+    private CommonConstant() {
+
+    }
+
+    public static final String COMMON_LOGGER = "common.logger";
+
     public static final String ERROR_LOGGER = "error.logger";
 
+    @Deprecated
     public static final String JDBC_LOGGER = "jdbc.logger";
 }
--- src/main/java/org/daming/jobs/base/exceptions/ExceptionBuilder.java
+++ src/main/java/org/daming/jobs/base/exceptions/ExceptionBuilder.java
@@ -1,6 +1,20 @@
 package org.daming.jobs.base.exceptions;
 
+import org.daming.jobs.base.logger.LoggerManager;
+
 public class ExceptionBuilder {
 
-    // public static DamingException()
+    public static DamingException buildException(String code, String message, Throwable clause) {
+        DamingException ex = new DamingException(code, message, clause);
+        logPointEngineException(ex);
+        return ex;
+    }
+
+    public static DamingException buildException(String code, String message) {
+        return buildException(code, message, null);
+    }
+
+    private static void logPointEngineException(DamingException e) {
+        LoggerManager.getErrorLogger().error(e.getCode() + ", " + e.getMessage(),e);
+    }
 }
