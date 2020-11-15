8ae326a030aafcab39ec0df1c4fbd10e98f93dbc
damingerdai <mingguobin@live.com>
2020-11-15 22:40:47 +0800

    feat: add api logger

--- src/main/java/org/daming/jobs/api/controller/JobController.java
+++ src/main/java/org/daming/jobs/api/controller/JobController.java
@@ -9,7 +9,14 @@ import org.daming.jobs.pojo.request.PauseResumeJobRequest;
 import org.daming.jobs.service.IQuartzService;
 import org.quartz.SchedulerException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
--- src/main/java/org/daming/jobs/base/constant/CommonConstant.java
+++ src/main/java/org/daming/jobs/base/constant/CommonConstant.java
@@ -10,7 +10,6 @@ public class CommonConstant {
 
     public static final String ERROR_LOGGER = "error.logger";
 
-    @Deprecated
     public static final String JDBC_LOGGER = "jdbc.logger";
 
     public static final String API_LOGGER = "api.logger";
