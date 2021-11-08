76c1757efd53876233ccf38b11a347599da08834
Arthur Ming <937765518@qq.com>
2021-11-08 20:21:23 +0800

    build: bump react router v6 and rename web view title (#8)
    * build: bump react router v6
    
    * feat: rename web view title

--- src/main/react/src/layout/primary-layout.tsx
+++ src/main/react/src/layout/primary-layout.tsx
@@ -1,14 +1,14 @@
 import React, { Suspense } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { ROUTES } from "../route";
 
 export const PrimaryLayout = () => {
   return (
     <main>
       <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          {ROUTES.map((route) => <Route key={route.path} exact path={route.path} component={route.component} />)}
-        </Switch>
+        <Routes>
+          {ROUTES.map((route) => <Route key={route.path} path={route.path} element={<route.component/>} />)}
+        </Routes>
       </Suspense>
     </main>
   );
