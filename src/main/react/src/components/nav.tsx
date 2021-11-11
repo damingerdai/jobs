1eb75ebff12ffdef03c1251aaae9c36c69caf1d6
Arthur Ming <guobin.ming@pwc.com>
2021-11-11 10:32:40 +0800

    refactor: use ThemeModePicker component to toggle theme mode

--- src/main/react/src/components/nav.tsx
+++ src/main/react/src/components/nav.tsx
@@ -3,24 +3,23 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded';
-import NightlightRoundedIcon from '@mui/icons-material/NightlightRounded';
 
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from '../slices/store';
 import { toggleMode } from '../slices/theme';
+import { ThemeMode } from '../types/theme';
+import { ThemeModePicker } from './themeModePicker';
 
 interface NavbarProps {
   brand: string,
-  toggleMode: (mode: 'light' | 'dark') => void;
+  toggleMode: (mode: ThemeMode) => void;
 }
 
 const Navbar: React.FC<NavbarProps> = (props) => {
-  const mode = useSelector((state: RootState) => state.theme.mode);
+  const mode = useSelector((state: RootState) => state.theme.mode) as ThemeMode;
   const dispatch = useDispatch()
 
   return (
@@ -31,7 +30,11 @@ const Navbar: React.FC<NavbarProps> = (props) => {
           <Button component={RouterLink} to="/" color="inherit" >Home</Button>
           <Button component={RouterLink} to="/repos" color="inherit" >Repos</Button>
           <Box component="div" sx={{ flexGrow: 1 }} />
-          {mode === 'light' && <IconButton aria-label="theme" color="inherit" onClick={() => {
+          <ThemeModePicker mode={mode} modeChange={(mode) => {
+             props.toggleMode(mode);
+             dispatch(toggleMode(mode));
+          }}/>
+          {/* {mode === 'light' && <IconButton aria-label="theme" color="inherit" onClick={() => {
             props.toggleMode('dark');
             dispatch(toggleMode('dark'));
           }}>
@@ -42,7 +45,7 @@ const Navbar: React.FC<NavbarProps> = (props) => {
             dispatch(toggleMode('light'));
           }}>
             <NightlightRoundedIcon />
-          </IconButton>}
+          </IconButton>} */}
           <Button component={RouterLink} to="/login" color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
---
+++ src/main/react/src/components/themeModePicker.tsx
@@ -0,0 +1,23 @@
+import IconButton from '@mui/material/IconButton';
+import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded';
+import NightlightRoundedIcon from '@mui/icons-material/NightlightRounded';
+
+import React from 'react';
+
+import { ThemeMode } from '../types/theme';
+
+interface themeModePickerProps {
+    mode: ThemeMode
+    modeChange: (mode: ThemeMode) => void;
+}
+
+export const ThemeModePicker: React.FC<themeModePickerProps> = (props) => {
+    const { mode, modeChange } = props
+    if (mode === 'dark') {
+        return <IconButton aria-label="theme" color="inherit"  onClick={() => modeChange('light')}><NightlightRoundedIcon/></IconButton>
+    } else if (mode === 'light') {
+        return <IconButton aria-label="theme" color="inherit" onClick={() => modeChange('dark')}><LightModeRoundedIcon/></IconButton>
+    } else {
+        return never;
+    }
+}
